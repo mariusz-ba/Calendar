b9fd9117c005b0402bf6e029dfbe8415a2654b67
unknown <mariusz.baran23@gmail.com>
2016-02-21 22:00:42 +0100

    made changes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/********************************************************
+ *
+ * Calendar App
+ *
+ * Created by Mariusz Baran on 17.02.2016
+ * Copyright (c) 2016 Mariusz Baran. All rights reserved
+ *
+ ********************************************************/
+
 #include "mainwindow.h"
 #include <QApplication>
 
